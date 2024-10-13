aef1a1db63dc084d98a76e6e0bf1845043ae30af
vyogi4 <vyogi4151@gmail.com>
2024-10-14 00:38:04 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-3xl underline">hola amigos</h1>
+    </div>
+  )
+}
+
+export default App
