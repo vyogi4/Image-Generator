3efd424d2ed45910d7b6f143bc73b5a882d00d08
vyogi4 <vyogi4151@gmail.com>
2024-10-14 15:09:57 +0530

    frontend setup

---
+++ client/src/utils/index.js
@@ -0,0 +1,13 @@
+import { surpriseMePrompts } from "../constant";
+
+
+export function getRandomPrompts(prompt) {
+
+    const randomIndex = Math.floor(Math.random()*surpriseMePrompts.length);
+
+    const randomPrompt = surpriseMePrompts[randomIndex]
+
+    if(randomPrompt ===  prompt) return getRandomPrompts(prompt);
+
+    return randomPrompt;
+}
